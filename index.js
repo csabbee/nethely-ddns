cbf1863599660a9fd3e1bb701cf6febd11f4f50f
Csaba Valyi <csaba.valyi1@gmail.com>
2025-03-12 07:08:23 +0000

    feat: add timestamp to logs

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 import dotenv from 'dotenv'
-dotenv.config();
+dotenv.config()
 
-const FIVE_MINUTES = 1000 * 60 * 5;
+const FIVE_MINUTES = 1000 * 60 * 5
 
 const { NETHELY_URL } = process.env
 
+const formatter = new Intl.DateTimeFormat('en-US', {
+  hour12: false,
+  hour: '2-digit',
+  minute: '2-digit',
+  second: '2-digit',
+  year: 'numeric',
+  month: '2-digit',
+  day: '2-digit',
+})
+
 setInterval(async () => {
-  const response = await fetch(NETHELY_URL);
+  const response = await fetch(NETHELY_URL)
+  const date = new Date()
 
   if (!response.ok) {
-    console.error('Error while updating ddns record');
+    console.error(`[${formatter.format(date)}] - Error while updating ddns record`)
   } else {
-    console.log('Update OK');
+    console.log(`[${formatter.format(date)}] - Update OK`)
   }
-}, FIVE_MINUTES);
+}, FIVE_MINUTES)
